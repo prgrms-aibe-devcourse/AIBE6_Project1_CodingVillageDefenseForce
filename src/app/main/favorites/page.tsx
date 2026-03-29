0e6123a68f11a1f825ee5622a65b4d87da4c360c
ysman <tke0329@gmail.com>
2026-03-29 21:52:48 +0900

    삭제버튼 및 기능 구현

--- src/app/main/favorites/page.tsx
+++ src/app/main/favorites/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Header from '@/components/layout/Header'
+import { createClient } from '@/lib/supabase/client'
 import { useEffect, useState } from 'react'
 
 interface Place {
